b6a2551439e0b0c32fc5740f1132234c482847fd
jongrah <150179348+jongrah@users.noreply.github.com>
2024-01-04 17:45:17 -0300

    add start e restart screen + player moviment

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -13,14 +13,49 @@ public class PlayerController : MonoBehaviour
     [Header("Player Settings")]
     public float speed = 1f;
 
+    [Header("Other Settings")]
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndLine = "EndLine";
+    private bool _canRun;
+    public GameObject endScreen;
+
     void Update()
     {
-        _pos = transform.position;
+        if (!_canRun) return;
+
+        _pos = target.position;
         _pos.y = transform.position.y;
         _pos.z = transform.position.z;
 
 
         transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEnemy)
+        {
+            EndGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == tagToCheckEndLine)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+
+    public void StartToRun()
+    {
+        _canRun = true;
     }
 }
